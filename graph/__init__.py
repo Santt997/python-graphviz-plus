55195cddf144a28f29e4c50aefe2e31b9327bd91
Santt997 <santiago.burastero@gmail.com>
2026-05-26 09:16:56 -0300

    add

--- graph/__init__.py
+++ graph/__init__.py
@@ -1,3 +1,6 @@
 from .black_neato_graph import BlackNeatoGraph
 from .valency_list import ValencyList
 from .adjacency_list import AdjacencyList
+
+# Esto le indica a Pylance que estas tres clases son exportaciones válidas
+__all__ = ['BlackNeatoGraph', 'AdjacencyList', 'ValencyList']
