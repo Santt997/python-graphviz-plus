889f9c342320f01e6f294170b3dc2f1c67cc295a
Santt997 <santiago.burastero@gmail.com>
2026-06-10 22:37:16 -0300

    add

--- graph_plus/adjacency_list.py
+++ graph_plus/adjacency_list.py
@@ -7,29 +7,29 @@ graphviz.__version__
 
 class AdjacencyList:
     def __init__(self, edges_list: list[tuple[str, str]]):
-        '''Constructor principal basado en una lista de aristas (ll).'''
+        '''MainConstructor based in 1 list[edges] (ll).'''
         self.edges = edges_list
 
     @classmethod
     def from_dict_of_tuples(cls, data_dict: dict[str, tuple[str, ...]]):
-        '''Constructor extra: genera la clase a partir de un dict de tuplas.'''
+        '''Constructor extra: genera the class a partir of 1 dict[tuple].'''
         edges_from_dict = []
-        # Recorremos el diccionario para armar la lista de aristas tradicionales (ll)
+        # Iterate the dict 4build the list[edges] traditionals (ll)
         for nodo_origen, vecinos in data_dict.items():
             for nodo_destino in vecinos:
-                # Evitamos duplicar aristas invirtiendo el orden (ej: si ya está B-A, no agregamos A-B)
+                # Avoid duplicate edges invirtiendo the order (ex: if ya is B-A, no add A-B)
                 if (nodo_destino, nodo_origen) not in edges_from_dict:
                     edges_from_dict.append((nodo_origen, nodo_destino))
         
-        # cls(...) llama al constructor __init__ original pasándole la lista que acabamos de armar
+        # cls(...) call 2constructor __init__ original passing the list that recently builded
         return cls(edges_list=edges_from_dict)
     
-    def get_ll(self) -> list:
-        '''Retorna la lista de aristas (ll) del grafo.'''
+    def get_ll(self) -> list[tuple[str, str]]:
+        '''-> list[edges] (ll) of graph.'''
         return self.edges
 
     def to_dataframe(self) -> pd.DataFrame:
-        '''Tu método exacto de conversión basado en NetworkX con soporte para .T'''
+        '''Tu mtd exact of conversion based in NetworkX w support for .T'''
         graph = nx.Graph(self.edges)
         dict_lists = nx.to_dict_of_lists(graph)
         df = pd.DataFrame.from_dict(dict_lists, orient='index').T
